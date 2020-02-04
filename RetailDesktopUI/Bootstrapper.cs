62b05590765b042f96029891c122561c2ee0fddb
Neal Thomas <mvc.programmer@gmail.com>
2020-02-04 15:56:07 -0500

    Simple Container Dependency Injection

--- RetailDesktopUI/Bootstrapper.cs
+++ RetailDesktopUI/Bootstrapper.cs
@@ -11,14 +11,33 @@ namespace RetailDesktopUI
 {
     public class Bootstrapper : BootstrapperBase
     {
+        private SimpleContainer _simpleContainer = new SimpleContainer();
         public Bootstrapper()
         {
             Initialize();
         }
 
-        protected override void OnStartup(object sender, StartupEventArgs e)
+        protected override void Configure()
         {
-            DisplayRootViewFor<ShellViewModel>();
+            _simpleContainer.Instance(_simpleContainer);
+            _simpleContainer
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+
+            GetType().Assembly.GetTypes()
+                .Where(t => t.IsClass)
+                .Where(t => t.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(viewModelType => _simpleContainer.RegisterPerRequest(
+                    viewModelType, viewModelType.ToString(), viewModelType));
         }
+
+        protected override void OnStartup(object sender, StartupEventArgs e) => DisplayRootViewFor<ShellViewModel>();
+       
+        protected override object GetInstance(Type service, string key) => _simpleContainer.GetInstance(service, key);
+        
+        protected override IEnumerable<object> GetAllInstances(Type service) => _simpleContainer.GetAllInstances(service);
+        
+        protected override void BuildUp(object instance) =>_simpleContainer.BuildUp(instance);
     }
 }
