5bdb2d17ab6188f108a7c5c30a41d2b56d777484
Neal Thomas <mvc.programmer@gmail.com>
2020-02-05 16:19:11 -0500

    Sales Page Designed

---
+++ RetailDesktopUI/ViewModels/SalesViewModel.cs
@@ -0,0 +1,89 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetailDesktopUI.ViewModels
+{
+    public class SalesViewModel : Screen
+    {
+		private BindingList<string> _products;
+
+		public BindingList<string> Products
+		{
+			get { return _products; }
+			set 
+			{
+				_products = value;
+				NotifyOfPropertyChange(() => Products);
+			}
+		}
+
+		private BindingList<string> _cart;
+
+		public BindingList<string> Cart
+		{
+			get { return _cart; }
+			set
+			{
+				_cart = value;
+				NotifyOfPropertyChange(() => Cart);
+			}
+		}
+
+
+		private string _itemQuantity;
+
+		public string ItemQuantity
+		{
+			get { return _itemQuantity; }
+			set 
+			{
+				_itemQuantity = value;
+				NotifyOfPropertyChange(() => ItemQuantity);
+			}
+		}
+
+		private int myVar;
+
+		public string SubTotal
+		{
+			get { return "$0.00"; }
+		}
+
+		public string Tax
+		{
+			get { return "$0.00"; }
+		}
+
+		public string Total
+		{
+			get { return "$0.00"; }
+		}
+
+
+		public bool CanAddToCard => false;
+
+		public void AddToCart()
+		{
+
+		}
+
+		public bool CanRemoveFromCard => false;
+
+		public void RemoveFromCart()
+		{
+
+		}
+
+		public bool CanCheckOut => false;
+
+		public void CheckOut()
+		{
+
+		}
+	}
+}
